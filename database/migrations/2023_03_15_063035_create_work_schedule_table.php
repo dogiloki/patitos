5f93d410a08d15256f53c2a03eea7dbae04f663d
Julio Villanueva <wolfri2002@gmail.com>
2023-03-15 00:44:31 -0600

    Creación de migraciones de DB, y pequeños cambios en la DB

---
+++ database/migrations/2023_03_15_063035_create_work_schedule_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('work_schedule', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('id_employee')->unsigned();
+            $table->dateTime('date_start');
+            $table->dateTime('date_end');
+            $table->timestamps();
+            $table->foreign('id_employee')->references('id')->on('employee');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('work_schedule');
+    }
+};
